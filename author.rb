d3c27c61cbf109cf094412d359e2f4b52cfae20d
github username <joshmrallen@gmail.com>
2020-07-20 10:27:35 -0400

    created author and books models and established one to many relationship through attributes and methods

--- author.rb
+++ author.rb
@@ -1,1 +1,54 @@
+class Author
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        Author.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.longest_book
+        verbose = Author.all.map do |author|
+            author.books.map do |book|
+                book.word_count
+            end.max
+        end
+        #use .index to find the index of the max from the verbose array
+        index = verbose.index(verbose.max)
+        #returns the author instance with the biggest book
+        Author.all[index]
+    end
+
+    def self.most_words
+        verbose = Author.all.map do |author|
+            author.total_words
+        end
+        index = verbose.index(verbose.max)
+        #returns the author instance who has written the most words in total
+        Author.all[index]
+    end
+
+    def books
+        Book.all.select do |book|
+            book.author == self
+        end
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, self, word_count)
+    end
+
+    def total_words
+        count = 0
+        books.map do |book|
+            count += book.word_count
+        end
+        count
+    end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,28 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+joyce = Author.new("James Joyce")
+adams = Author.new("Ansel Adams")
+picard = Author.new("Jean-luke Picard")
+clemans = Author.new("Samuel Clemans")
+
+joyce.write_book("Ulyses", 500)
+joyce.write_book("Finnegan's Wake", 320)
+adams.write_book("Don't Stop the F-Stop", 720)
+adams.write_book("See It All", 600)
+adams.write_book("Rounded Stones Under a Mountain", 400)
+picard.write_book("The Unknown", 900)
+picard.write_book("Boldly Going", 400)
+picard.write_book("Wrinkly Foreheads", 590)
+picard.write_book("Makeup", 325)
+picard.write_book("Voyaging", 150)
+clemans.write_book("Huckleberry Finn", 320)
+clemans.write_book("The Frog Jumping Contest", 150)
+clemans.write_book("The Mighty Mississippi", 400)
+clemans.write_book("Tom & Huck", 560)
+clemans.write_book("A Connecticut Yankee in King Arthur's Court", 740)
+
+
 binding.pry
 
 
